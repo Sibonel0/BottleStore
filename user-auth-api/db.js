2131ccb8f10fd7e4382d850672fd9d3a916ce2ff
Sibonel0 <sibonelodlamini25@gmail.com>
2025-06-05 15:03:45 +0200

    Update db.js

--- user-auth-api/db.js
+++ user-auth-api/db.js
@@ -7,7 +7,7 @@ const { Pool } = require('pg');
 require('dotenv').config();
 const isRender = Boolean(process.env.DATABASE_URL);
 const pool = new Pool({
-    connectionString:  process.env.DATABASE_URL|| "postgresql://postgres:Sibonelo25!@localhost:5432/BottleStore",
+    connectionString:  process.env.DATABASE_URL|| "postgresql://sibonelo:e8k8ZvzfmaV7n2mPR3cPrVwtypeuOx5X@dpg-d10luf49c44c73drgntg-a/bottlestore_fkbq",
     ssl: isRender
     ? {rejectUnauthorized: false} //Required for render
     : false, //No SSL for local
